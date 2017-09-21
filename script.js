e341291b5210eb12073c4f27579ff1d8d5507ea7
Mike Roest <99040748@mydavinci.nl>
2017-09-21 12:14:11 +0200

    Afbeeldingen toevoegen voor de levels

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById('startgame').innerHTML = 'Play!';
 document.getElementById('level_title').innerHTML = 'Start het spel';
 
 var HasKey = false;
+
+function Level1() {
+	console.log("Level1()");
+
+	var opt1 = document.getElementById('option1');
+}
