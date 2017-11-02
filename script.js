6b8959eba44e242b4f394139bf5cdba07e69bfd9
Mike Roest <99040748@mydavinci.nl>
2017-11-02 09:46:35 +0100

    -

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function level3() {
 }
 
 function level4() {
-	document.getElementById('text').innerHTML ='Welke kant ga je po';	
+	document.getElementById('text').innerHTML ='Welke kant ga je op?';	
 	document.body.style.background="url('img/lvl4.jpg')";
 	document.body.style.backgroundSize='cover';
 	document.getElementById('knop1').style.display="none";
@@ -91,6 +91,114 @@ function level4() {
 	document.getElementById('knop2b').style.display="none";
 	document.getElementById('knop3').style.display="none";
 	document.getElementById('knop4').style.display="none";
+	document.getElementById('knop5a').style.display="block";
+	document.getElementById('knop5b').style.display="none";
+	document.getElementById('knop6').style.display="block";
+	document.getElementById('keystone2').style.display="none";
 
 	console.log("Level 4!")
+}
+
+function level5() {
+	document.getElementById('text').innerHTML ='Je bent richting het westen gegaan. Je komt aan in bij een grot. Het voelt duister en koud.'
+	document.body.style.background="url('img/west.jpg')";
+	document.body.style.backgroundSize='cover';
+	document.getElementById('knop1').style.display="none";
+	document.getElementById('knop2a').style.display="none";
+	document.getElementById('knop2b').style.display="none";
+	document.getElementById('knop3').style.display="none";
+	document.getElementById('knop4').style.display="none";
+	document.getElementById('knop5a').style.display="none";
+	document.getElementById('knop5b').style.display="block";
+	document.getElementById('knop5c').style.display="none";
+	document.getElementById('knop5d').style.display="none";
+	document.getElementById('knop6').style.display="none";
+	document.getElementById('keystone2').style.display="none";
+}
+
+function lvl5boss() {
+	document.getElementById('text').innerHTML ='Je treft de boze Hutt Anreb aan! Je kunt niet ontkomen en gaat het gevecht aan.'
+	document.body.style.background="url('img/west2a.jpg')";
+	document.body.style.backgroundSize='cover';
+	document.getElementById('knop1').style.display="none";
+	document.getElementById('knop2a').style.display="none";
+	document.getElementById('knop2b').style.display="none";
+	document.getElementById('knop3').style.display="none";
+	document.getElementById('knop4').style.display="none";
+	document.getElementById('knop5a').style.display="none";
+	document.getElementById('knop5b').style.display="none";
+	document.getElementById('knop5c').style.display="block";
+	document.getElementById('knop6').style.display="none";
+	document.getElementById('keystone2').style.display="none";
+}
+
+function anrebwin() {
+	document.getElementById('text').innerHTML ='Je hebt Anreb gedood! Bij het overwinnen heb je een kracht erbij gekregen.'
+	document.body.style.background="url('img/enrabdead.jpg')";
+	document.body.style.backgroundSize='cover';
+	document.getElementById('knop1').style.display="none";
+	document.getElementById('knop2a').style.display="none";
+	document.getElementById('knop2b').style.display="none";
+	document.getElementById('knop3').style.display="none";
+	document.getElementById('knop4').style.display="none";
+	document.getElementById('knop5a').style.display="none";
+	document.getElementById('knop5b').style.display="none";
+	document.getElementById('knop5c').style.display="none";
+	document.getElementById('knop5d').style.display="none";
+	document.getElementById('knop5e').style.display="block";
+	document.getElementById('knop6').style.display="none";
+	document.getElementById('keystone2').style.display="none";
+}
+
+function anrebfail() {
+	document.getElementById('text').innerHTML ='Je hebt gefaald, je bent dood. Druk op F5 om opnieuw te beginnen'
+	document.body.style.background="url('img/dood.jpg')";
+	document.body.style.backgroundSize='cover';
+	document.getElementById('knop1').style.display="none";
+	document.getElementById('knop2a').style.display="none";
+	document.getElementById('knop2b').style.display="none";
+	document.getElementById('knop3').style.display="none";
+	document.getElementById('knop4').style.display="none";
+	document.getElementById('knop5a').style.display="none";
+	document.getElementById('knop5b').style.display="none";
+	document.getElementById('knop5c').style.display="none";
+	document.getElementById('knop5d').style.display="none";
+	document.getElementById('knop5e').style.display="none";
+	document.getElementById('knop6').style.display="none";
+	document.getElementById('keystone2').style.display="none";
+}
+
+function level6a() {
+	document.getElementById('text').innerHTML ='Je komt aan bij een portaal wat nog niet geopend is. Keer later terug om verder te gaan'
+	document.body.style.background="url('img/zuid.jpg')";
+	document.body.style.backgroundSize='cover';
+	document.getElementById('knop1').style.display="none";
+	document.getElementById('knop2a').style.display="none";
+	document.getElementById('knop2b').style.display="none";
+	document.getElementById('knop3').style.display="none";
+	document.getElementById('knop4').style.display="none";
+	document.getElementById('knop5a').style.display="none";
+	document.getElementById('knop5b').style.display="none";
+	document.getElementById('knop5c').style.display="none";
+	document.getElementById('knop5c').style.display="none";
+	document.getElementById('knop6a').style.display="none";
+	document.getElementById('keystone2').style.display="none";
+}
+
+function level6b() {
+	document.getElementById('text').innerHTML ='Het portaal is geopend.'
+	document.body.style.background="url('img/zuid.jpg')";
+	document.body.style.backgroundSize='cover';
+	document.getElementById('knop1').style.display="none";
+	document.getElementById('knop2a').style.display="none";
+	document.getElementById('knop2b').style.display="none";
+	document.getElementById('knop3').style.display="none";
+	document.getElementById('knop4').style.display="none";
+	document.getElementById('knop5a').style.display="none";
+	document.getElementById('knop5b').style.display="none";
+	document.getElementById('knop5c').style.display="none";
+	document.getElementById('knop5d').style.display="none";
+	document.getElementById('knop5e').style.display="none";
+	document.getElementById('knop6a').style.display="none";
+	document.getElementById('keystone2').style.display="none";
 }
